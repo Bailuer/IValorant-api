aa8802e8f2459e2437400513c89f4e0bf2d5c29a
Bailuer <64820294+Bailuer@users.noreply.github.com>
2023-02-26 11:58:30 +0800

    Update api.py

--- api.py
+++ api.py
@@ -5,14 +5,7 @@ app = Flask(__name__)
 def login():
     usernames = request.form.get("username",None)
     passwords = request.form.get("password",None)
-    print(usernames,passwords)
     apiresult = index.main(usernames,passwords)
     return jsonify(apiresult)
-    '''
-    if(index.auth.Auth.if2fa):
-        return jsonify("please return the vc")
-    else:
-        return jsonify(apiresult)
-    '''
 if __name__ == '__main__':
     app.run(debug=True)
